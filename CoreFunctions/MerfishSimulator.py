06e3df2c3100a7cb6c204302ab7bc4df35993ca6
rlittman16 <rlittman16@g.ucla.edu>
2020-09-17 08:41:02 -0700

    removed umap from imported files

--- CoreFunctions/MerfishSimulator.py
+++ CoreFunctions/MerfishSimulator.py
@@ -11,7 +11,6 @@ import sys
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import scale
 from sklearn.linear_model import LogisticRegression
-from umap import UMAP
 from sklearn.model_selection import train_test_split
 
 class merfish_data_generator:
