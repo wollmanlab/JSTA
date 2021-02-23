1337a66b818884ba25146151e5a621ba75b681c1
Russell <rlittman16@g.ucla.edu>
2021-02-23 13:11:44 -0800

    added tutorial on subsetting to marker genes in RunningJSTA.ipynb

--- CoreFunctions/JSTA.py
+++ CoreFunctions/JSTA.py
@@ -19,7 +19,7 @@ from tensorflow.keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
 from sklearn import neighbors
 
-path_to_file = #REPLACE-WITH-PATH
+path_to_file = "/home/russell/hoffman/JSTA/CoreFunctions"
 
 def find_empty_pixels(pix):
     '''
